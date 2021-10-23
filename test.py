e636a813216a9e9d4a5e6e02a2625e2b362b0810
Gheorghe Dontu <gheorghedontu@Gheorghes-MBP.home>
2021-10-23 20:49:26 +0100

    am adaugat test.py

---
+++ test.py
@@ -0,0 +1,14 @@
+x = 10
+if x > 5: # True
+    if x == 6: # False
+        print("nested: x == 6")
+    elif x == 10: # True
+        print("nested: x == 10")
+    else:
+        print("nested: else")
+else:
+    print("else")
+
+    
+    names = ["Gigi", "alina", "se fut"]
+    print (names)
