2323519e167d0132f604d3f3cc900af52727d3c8
gigione22 <dontugheorghe@yahoo.co.uk>
2025-12-29 18:12:13 +0000

    update si gigione.py

--- gigione.py
+++ gigione.py
@@ -1,4 +1,4 @@
-
+ 
 calculation_of_unit = 24
 name_of_unit = "hours"
 
@@ -6,7 +6,7 @@ def day_to_units(num_of_days):
          print(f"{num_of_days} day are {calculation_of_unit*num_of_days} {name_of_unit}")
 
 gigione = input("hey user intro un numar care sa fie convertiti \n")
-print(gigione)
+print("Gigione")
 print(type('gigione'))
 
 
